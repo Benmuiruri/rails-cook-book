9c1889415ce0ffb89568f0935659204a5fbbbfb1
Benmuiruri <ben.muiruri13@gmail.com>
2022-07-26 17:22:53 +0300

    Add create action to handle adding new recipe
    Update flash message

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -10,4 +10,21 @@ class RecipesController < ApplicationController
   def new
     @recipe = Recipe.new
   end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    @recipe.user = current_user
+    if @recipe.save
+     redirect_to recipe_path(@recipe.id), notice: 'New recipe created successfully.'
+    else
+      flash[:alert] = 'Something went wrong, recipe not created'
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  
+  def recipe_params
+    params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description, :public)
+  end
 end
