3f4eec392b2f1009b3d728631c683ee5166de4f3
Ezekielutshudi <ezekielutshudi@gmail.com>
2022-07-26 10:20:53 +0200

    add the assosciation food to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :recipes
-
+  has_many :foods
   validates :name, presence: true, length: { in: 3..10 }
 end
