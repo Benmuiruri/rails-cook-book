75794011f163b92d69cba4e0f8c246ca5a0d8bff
Ezekielutshudi <ezekielutshudi@gmail.com>
2022-07-26 17:39:39 +0200

    add food controllers

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -6,4 +6,33 @@ class FoodsController < ApplicationController
       redirect_to user_session_path
     end
   end
+
+  def new
+    @food = Food.new
+  end
+
+  def create
+    @food = current_user.foods.build(food_params)
+
+    if @food.save
+      redirect_to foods_url, notice: 'Food was successfully created.'
+    else
+      render :new, alert: 'Food could not be created'
+    end
+  end
+
+  def destroy
+    @food.destroy
+    redirect_to foods_url, notice: 'Food was successfully destroyed.'
+  end
+
+  private
+
+  def set_food
+    @food = Food.find(params[:id])
+  end
+
+  def food_params
+    params.require(:food).permit(:name, :measurement_unit, :price)
+  end
 end
