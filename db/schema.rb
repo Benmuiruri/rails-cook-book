b05f7327eabcc20d3da578fb793eb603dafc1602
Benmuiruri <ben.muiruri13@gmail.com>
2022-07-26 11:35:24 +0300

    Add default values for recipes table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_26_082452) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_26_083323) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -37,10 +37,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_26_082452) do
 
   create_table "recipes", force: :cascade do |t|
     t.string "name"
-    t.integer "preparation_time"
-    t.integer "cooking_time"
+    t.integer "preparation_time", default: 0
+    t.integer "cooking_time", default: 0
     t.text "description"
-    t.boolean "public"
+    t.boolean "public", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
