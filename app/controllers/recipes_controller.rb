ec87b4d36c82387942714b9203b4cb2029299854
Benmuiruri <ben.muiruri13@gmail.com>
2022-07-26 16:35:00 +0300

    Add post show action

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,4 +2,9 @@ class RecipesController < ApplicationController
   def index 
     @recipes = Recipe.includes(:user).where(user_id: current_user.id)
   end
+
+  def show
+    @recipe = Recipe.includes(:user).find(params[:id])
+
+  end
 end
