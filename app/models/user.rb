db7ded57a6e7edf0a67fa531ae8c0a3a4bdebb08
Benmuiruri <ben.muiruri13@gmail.com>
2022-07-26 10:53:08 +0300

    Generate Recipe model, add association to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :recipes
 
   validates :name, presence: true, length: { in: 3..10 }
 end
