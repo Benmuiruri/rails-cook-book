626226fb1c7a2f6548f59ec8c03723b91de09c75
Benmuiruri <ben.muiruri13@gmail.com>
2022-07-27 10:01:17 +0300

    Add validations for recipe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,6 +3,9 @@ class Recipe < ApplicationRecord
   has_many :recipe_foods
 
   validates :name, presence: true
+  validates :preparation_time, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :cooking_time, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :description, presence: true
 
   def public_recipes
     @recipes = Recipe.where(public: true)
