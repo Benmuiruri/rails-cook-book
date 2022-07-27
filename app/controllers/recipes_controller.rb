ecb8cf4f239452225243a10c84ac844aaeab6ebf
Benmuiruri <ben.muiruri13@gmail.com>
2022-07-27 15:39:37 +0300

    Fix Rubocop offenses

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,5 @@
 class RecipesController < ApplicationController
-   before_action :authenticate_user!, except: :index
+  before_action :authenticate_user!, except: :index
 
   def index
     @recipes = Recipe.all
