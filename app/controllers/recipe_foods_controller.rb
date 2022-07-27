7db35cf98ddac689f1a4613248d68d347614b310
Benmuiruri <ben.muiruri13@gmail.com>
2022-07-27 10:58:06 +0300

    Add Recipe Food create action, add ingredients partial

--- app/controllers/recipe_foods_controller.rb
+++ app/controllers/recipe_foods_controller.rb
@@ -1,7 +1,28 @@
 class RecipeFoodsController < ApplicationController
+  before_action :authenticate_user!
+
   def create
+    if recipe.user == current_user
+      new_recipe_food = RecipeFood.new(recipe_food_params)
+      recipe = Recipe.find(params[:recipe_id])
+      new_recipe_food.recipe = recipe
+      if new_recipe_food.save
+        redirect_to recipe_path(recipe), notice: 'Food added to recip successfully.'
+      else
+        flash[:alert] = 'Something went wrong, food not added to recipe'
+      end
+    else
+      flash[:alert] = 'Permission to add new food denied'
+    end
+     redirect_to recipe_path(recipe)
   end
 
   def destroy
+  end
+  
+  private
+
+  def recipe_food_params
+    params.require(:recipe_food).permit(:food_id, :recipe_id, :quantity)
   end
 end
