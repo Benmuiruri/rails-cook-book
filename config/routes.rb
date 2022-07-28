9e0c37034937cc619f3fffe9d2fa8a33e7a5dc6c
Ezekielutshudi <ezekielutshudi@gmail.com>
2022-07-28 10:43:14 +0200

    Add the header for the logged-in user
    Changed the my-recipes route

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
     resources :recipe_foods, only: [:create, :destroy]
   end
   resources :foods, except: [:update]
-  get 'my_recipes', to: 'recipes#index'
+  get 'my_recipes', to: 'recipes#my_recipes'
   get 'public_recipes', to: 'recipes#public_recipes'
   resources :general_shopping_list, only: [:index]
 end
